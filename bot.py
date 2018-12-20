f82febc410db3b7406d3ba9cb3ab6b1af3eb59a9
Dmitry Y <hds101@gmail.com>
2018-12-20 15:17:02 +0300

    Fix sosna uid

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ async def on_message(message):
         )
 
     elif message.content.startswith('<!'):
-        sosna = message.guild.get_member('0014')
+        sosna = message.guild.get_member(188000465550573569)
         await message.channel.send('{}, https://github.com/Vitalyii/JS/commit/780f5440e2114ec86872bff991082d2a88deb39a'.format(sosna.mention))
 
     elif message.content.startswith('!comeon'):
