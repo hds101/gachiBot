0a7571217896d5720c9bdc64171197072f7032de
Dmitry Y <hds101@gmail.com>
2018-12-22 12:19:12 +0300

    Some txt commands

--- bot/text_bot.py
+++ bot/text_bot.py
@@ -10,4 +10,20 @@ class TextBot:
         """ Zaebat' sosninu """
 
         sosna = ctx.guild.get_member(188000465550573569)
-        await ctx.send(sosna.mention)
+        await ctx.send('И охуенен 👉 {}'.format(sosna.mention))
+
+    @commands.command()
+    async def pubg(self, ctx):
+        """ Let's play some pubg """
+        pass
+
+    @commands.command()
+    async def pupk(self, ctx):
+        """ !pubg alias """
+        pass
+
+    @pubg.before_invoke
+    @pupk.before_invoke
+    async def __pubg(self, ctx):
+        gav = self.bot.get_emoji(485168219926167555)
+        await ctx.send('{0} {0} {0}'.format(gav))
