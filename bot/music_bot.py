28f24ced70e45af2f7e6978ff4f3c6082c1d6aac
Dmitry Y <hds101@gmail.com>
2019-04-05 15:07:22 +0300

    Try to disable ytdl streaming

--- bot/music_bot.py
+++ bot/music_bot.py
@@ -92,7 +92,7 @@ class MusicBot:
             player = await YTDLSource.from_url(
                 url,
                 loop=self.bot.loop,
-                stream=True,
+                stream=False,
                 volume=self.volume_lvl
             )
             ctx.voice_client.play(
